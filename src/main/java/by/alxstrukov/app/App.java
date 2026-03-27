d2a1ba14cb26df331636808e576e095f98d42de3
Alexandr Strukov <alxstrukov@yandex.ru>
2026-03-27 16:53:27 +0300

    Язык HQL. Обновление данных

--- src/main/java/by/alxstrukov/app/App.java
+++ src/main/java/by/alxstrukov/app/App.java
@@ -12,16 +12,39 @@ public class App {
         Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
 
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {
-            Session session = sessionFactory.getCurrentSession();
-
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.getCurrentSession()) {
+            
+            /*ВАЖНО - session и sessionFactory открывать в блоке try(session, sessionFactory){}
+            чтобы эти ресурсы потом закрылись*/
+            
             session.beginTransaction();
 
-            //Hibernate работает не с таблицами,а с java-классами (сущности @Entity)
-            int minAge = 50;
-            String hqlQuery = "from Person where age > " + minAge;//hql - вывести всех людей у которых возраст > 50
-            List<Person> personList = session.createQuery(hqlQuery, Person.class).getResultList();
-            personList.forEach(System.out::println);
+            //Hibernate работает не с таблицами, а с java-классами (сущности @Entity)
+
+            String hqlQuery = "update Person set name=:newName where age >:targetAge";
+
+            /*hqlQuery запрос обновит столбец (поле) name всем строкам в БД в таблице people
+            (но помним что Hibernate работает не с БД, а с java-сущностями,
+            * то есть, он знает что таблицу people из БД в java представляет класс Person
+            * помеченный аннотациями @Entity и @Table (name = "people") */
+
+            int updateCount = session.createQuery(hqlQuery)
+                    .setParameter("newName", "Some name")
+                    .setParameter("targetAge", 50)
+                    .executeUpdate();
+
+            /*При выполнении hql-запросов на обновление/удаление данных, в метод createQuery() не нужно передавать
+            * имя класса сущности (например Person.class) как при запросах типа /select * from Person/
+            * нужно передать только сам запрос, в котором указать имена параметров (:newName, :newAge)
+            *  для условий (where=...) и изменений (set=...)
+            * затем после метода createQuery() вызвать нужное кол-во раз метод setParameter()
+            * в который в качестве аргументов передать имя нужного параметра и его значение
+            * после чего вызвать метод executeUpdate();
+            * */
+
+            System.out.println(updateCount);//выведет в консоль кол-во обновленных элементов
+
 
             session.getTransaction().commit();//сохраняем транзакцию
         } catch (Exception e) {
